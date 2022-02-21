4d5d2a1b1b66cba8e60833c7d8365835a4359a54
Galdric Pujol <pujol;galdric@gmail.com>
2022-02-21 12:31:39 +0100

    LED clignote avec blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,17 @@
 #include <Arduino.h>
 #define LED 13
+int i = 0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED, OUTPUT);
 }
 
 void loop() {
+  Serial.println("Blink");
+  //i += 100;
   digitalWrite(LED, HIGH);
-  delay(1000);
+  delay(1000 - i);
   digitalWrite(LED, LOW);
-  delay(1000);
+  delay(1000 - i);
 }
